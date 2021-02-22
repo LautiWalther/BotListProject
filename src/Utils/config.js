e0d07eba977fe729c45a02148bcbb2136d5b4f41
LautiW <56508022+LautiWalther@users.noreply.github.com>
2021-02-22 18:58:09 -0300

    Update config.js

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "token": "NzMzNDU1NzAzNTQwMzY3NDQw.XxDZ2g.p0Sqc1Shlme3wsBrU4Krmtbx55I",
-  "id": "733455703540367440",
-  "clientSecret": "vojFSFJdhsmKe7T7xKHrQgPC5peKQHTs",
+  "token": "",
+  "id": "",
+  "clientSecret": "",
   "domain": "http://localhost/",
   "redirect_uri": "http://localhost/callback",
-  "server": "755187194234404954",
+  "server": "",
   "port": 80
 };
